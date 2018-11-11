e70358379dcd9361391f8eac8d0ab6e459ab8a45
kimgayeon <yellow139@naver.com>
2018-11-11 11:50:52 +0900

    update

---
+++ Roll_A_Ball_1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+
+    Vector3 velocity;
+    Rigidbody myRigidbody;
+
+    public float turnSpeed = 3f;
+    public float moveSpeed = 5f;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+        transform.Translate(0f, 0f, v * moveSpeed * Time.deltaTime); 
+        transform.Rotate(0f, h * turnSpeed * Time.deltaTime, 0f);
+    }
+
+}
